461c9e8967a583f9ba929bedbe5c5d19f7585191
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2018-10-10 21:47:01 +0600

    Add files via upload

---
+++ VERSTAK/VERSTAK/IronStick.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VERSTAK
+{
+    public class IronStick
+    {
+        public int Volume { get; set; }
+        public int Mass { get; set; }
+        public List<int> numList = new List<int>();
+
+
+
+        public void AddNum(int num)
+        {
+            numList.Add(num);
+        }
+        public static bool operator ==(IronStick st1, IronStick st2)
+
+        {
+            if (st1.Volume == st2.Volume)
+            {
+                return true;
+            }
+            else if (st1.Volume != st2.Volume)
+            {
+                return false;
+            }
+            return false;
+        }
+        public static bool operator !=(IronStick st1, IronStick st2)
+        {
+            if (st1.Volume != st2.Volume)
+            {
+                return true;
+            }
+            else if (st1.Volume == st2.Volume)
+            {
+                return false;
+            }
+            return false;
+        }
+        public static bool operator <(IronStick firsList, IronStick secondList)
+        {
+            return (firsList.numList.Sum() < secondList.numList.Sum()) ? true : false;
+        }
+
+        public static bool operator >(IronStick firsList, IronStick secondList)
+        {
+            return (firsList.numList.Sum() > secondList.numList.Sum()) ? true : false;
+        }
+
+    }
+}
---
+++ VERSTAK/VERSTAK/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VERSTAK
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            IronStick ironStick = new IronStick();
+            IronStick ironStick2 = new IronStick();
+            ironStick.Volume = 5;
+            ironStick2.Volume = 5;
+        
+
+            if(ironStick.Volume == ironStick2.Volume)
+            {
+                Console.WriteLine("Значения равны!");
+            }
+            else if (ironStick.Volume != ironStick2.Volume)
+            {
+                Console.WriteLine("Значения не равны");
+            }
+           
+            
+
+            Console.ReadLine();
+            Console.Clear();
+            SecondTask();
+            Console.ReadLine();
+        }
+        static void SecondTask()
+        {
+            IronStick firstList = new IronStick();
+            IronStick secondList = new IronStick();
+            firstList.AddNum(1);
+            firstList.AddNum(8);
+            secondList.AddNum(1);
+            secondList.AddNum(3);
+
+            Console.WriteLine((firstList > secondList) ? "Сумма первого массива больше" : "Сумма второго массива больше");
+        }
+    }
+}
