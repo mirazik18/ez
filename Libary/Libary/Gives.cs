75c4169b54db167f4d09f023275b5e6068d64260
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2019-01-27 20:29:55 +0600

    Add files via upload

---
+++ Libary/Libary/Gives.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libary
+{
+   public class Gives
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int ReaderId { get; set; }
+
+    }
+}
---
+++ Libary/Libary/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace Libary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DataSet dataSet = new DataSet("libaryDataBase");
+            DataTable authorTable = new DataTable("author");
+            DataColumn authorIdColumn = new DataColumn();
+            authorIdColumn.AutoIncrementSeed = 1;
+            authorIdColumn.AutoIncrementStep = 1;
+            authorIdColumn.AutoIncrement = true;
+            authorIdColumn.Unique = true;
+            authorIdColumn.ColumnName = "id";
+            authorIdColumn.DataType = typeof(int);
+            authorTable.Columns.Add(authorIdColumn);
+
+            DataColumn authorNameColumn = new DataColumn("FullName");
+            authorNameColumn.DataType = typeof(string);
+            authorTable.Columns.Add(authorNameColumn);
+
+
+            DataTable bookTable = new DataTable("books");
+            DataColumn bookIdColumn = new DataColumn();
+            bookIdColumn.AutoIncrementSeed = 1;
+            bookIdColumn.AutoIncrementStep = 1;
+            bookIdColumn.AutoIncrement = true;
+            bookIdColumn.Unique = true;
+            bookIdColumn.ColumnName = "id";
+            bookIdColumn.DataType = typeof(int);
+            bookTable.Columns.Add(bookIdColumn);
+
+            DataColumn bookNameColumn = new DataColumn("Name");
+            bookNameColumn.DataType = typeof(string);
+            bookTable.Columns.Add(bookNameColumn);
+
+            DataColumn bookAuthorIdColumn = new DataColumn("AuthorId");
+            bookAuthorIdColumn.DataType = typeof(int);
+
+            DataRelation dataRelation = new DataRelation("BookAuthorFK", "author", "books", new string[] { "id" }, new string[] { "AuthorId" }, false);
+        }
+    }
+}
