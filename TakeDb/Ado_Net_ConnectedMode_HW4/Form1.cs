d86c61b2a3fa11a3f6126c2a385a00d4d8cbdf7a
mirazik18 <40587386+mirazik18@users.noreply.github.com>
2019-01-28 18:01:34 +0600

    Add files via upload

---
+++ TakeDb/Ado_Net_ConnectedMode_HW4/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace Ado_Net_ConnectedMode_HW4
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection sqlConnection;
+        SqlCommand sqlCommand;
+        SqlDataReader sqlDataReader;
+        string connection;
+        public Form1()
+        {
+            connection = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            InitializeComponent();
+            AddData();
+        }
+
+        private void AddData()
+        {
+            sqlConnection = new SqlConnection(connection);
+            string Select = "select table_name from INFORMATION_SCHEMA.TABLES where TABLE_TYPE = 'base table' and table_name not like 'sys%';";
+
+            try
+            {
+                sqlConnection.Open();
+                sqlCommand = new SqlCommand(Select, sqlConnection);
+                sqlDataReader = sqlCommand.ExecuteReader();
+                while (sqlDataReader.Read())
+                {
+                    comboBox1.Items.Add(sqlDataReader["table_name"]);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                sqlConnection?.Close();
+                sqlCommand?.Clone();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlDataAdapter Adapter = new SqlDataAdapter("Select * from "+ comboBox1.Text, sqlConnection);
+            DataSet dataSet = new DataSet();
+            dataGridView1.DataSource = null;
+            Adapter.Fill(dataSet);
+            dataGridView1.DataSource = dataSet.Tables[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
